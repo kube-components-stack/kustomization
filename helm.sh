d7328116843e3a1863bb52edc7c6069045d7452f
Nicolas COLLET <MalibuKoKo@users.noreply.github.com>
2022-12-02 14:01:12 +0100

    refactor: add ns

--- helm.sh
+++ helm.sh
@@ -10,14 +10,16 @@ helm template \
   --namespace kube-system \
   --set env.fromValues.CLUSTER=plato \
   --set env.fromValues.ENV=devncoargo \
-  deploy/charts/kustomization |yq e -P
+  deploy/charts/kustomization > /tmp/kustomization.yaml
+  
+# cat /tmp/kustomization.yaml |yq e -P
 
-# helm uninstall kustomization \
-#   --namespace kube-system
+helm uninstall kustomization \
+  --namespace kube-system
 
-helm upgrade \
-  --install kustomization deploy/charts/kustomization \
-  --namespace kube-system \
-  --set env.fromValues.CLUSTER=plato \
-  --set env.fromValues.ENV=devncoargo \
-  -i
+# helm upgrade \
+#   --install kustomization deploy/charts/kustomization \
+#   --namespace kube-system \
+#   --set env.fromValues.CLUSTER=plato \
+#   --set env.fromValues.ENV=devncoargo \
+#   -i
