34618b7b364da638f48e6be79a8bf4df5a2bee9b
Nicolas COLLET <MalibuKoKo@users.noreply.github.com>
2022-12-02 17:26:54 +0100

    feat: remove cluster role after job

--- helm.sh
+++ helm.sh
@@ -4,22 +4,21 @@ kubens kube-system
 
 # helm ls --all
 
-helm template \
-  --debug \
-  --name-template=kustomization \
-  --namespace kube-system \
-  --set env.fromValues.CLUSTER=plato \
-  --set env.fromValues.ENV=devncoargo \
-  deploy/charts/kustomization > /tmp/kustomization.yaml
-  
-# cat /tmp/kustomization.yaml |yq e -P
-
-helm uninstall kustomization \
-  --namespace kube-system
-
-# helm upgrade \
-#   --install kustomization deploy/charts/kustomization \
+# helm template \
+#   --debug \
+#   --name-template=kustomization \
 #   --namespace kube-system \
 #   --set env.fromValues.CLUSTER=plato \
 #   --set env.fromValues.ENV=devncoargo \
-#   -i
+#   deploy/charts/kustomization > /tmp/kustomization.yaml
+  
+# cat /tmp/kustomization.yaml |yq e -P
+
+# helm uninstall kustomization \
+#   --namespace kube-system
+
+helm upgrade \
+  --install kustomization deploy/charts/kustomization \
+  --namespace kube-system \
+  --set env.fromValues.CLUSTER=plato \
+  --set env.fromValues.ENV=devncoargo
