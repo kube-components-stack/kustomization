cf151945e72ad8be41fbcb93f486fe17d13e64ea
Nicolas COLLET <MalibuKoKo@users.noreply.github.com>
2022-12-02 23:22:57 +0100

    refactor: add configmap

--- helm.sh
+++ helm.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-kubens kube-system
+# kubens kube-system
 
 # helm ls --all
 
-# helm template \
-#   --debug \
-#   --name-template=kustomization \
-#   --namespace kube-system \
-#   --set env.fromValues.CLUSTER=plato \
-#   --set env.fromValues.ENV=devncoargo \
-#   deploy/charts/kustomization > /tmp/kustomization.yaml
-  
-# cat /tmp/kustomization.yaml |yq e -P
+helm template \
+  --debug \
+  --name-template=kustomization \
+  --namespace kube-system \
+  --set env.fromValues.CLUSTER=plato \
+  --set env.fromValues.ENV=devncoargo \
+  deploy/charts/kustomization > /tmp/kustomization.yaml
+
+cat /tmp/kustomization.yaml |yq e -P
 
 # helm uninstall kustomization \
 #   --namespace kube-system
